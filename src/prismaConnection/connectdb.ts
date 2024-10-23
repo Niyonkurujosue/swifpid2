640f50e08aec9307719dd0f62e1675ef7eef8a4c
niyonkurujoshua2 <niyonkurujoshua2@gmail.com>
2024-10-23 16:18:51 +0000

    first commit

---
+++ src/prismaConnection/connectdb.ts
@@ -0,0 +1,20 @@
+// Suggested code may be subject to a license. Learn more: ~LicenseLog:2229951740.
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+async function main() {
+  // ... you will write your Prisma Client queries here
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
+
+export default prisma;
